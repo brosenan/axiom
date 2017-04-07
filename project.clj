9f3abfef5503f044fb5ce7cc978a31fd3a60c7c3
Boaz Rosenan <brosenan@gmail.com>
2017-04-07 09:34:24 +0300

    monolith plugin

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:plugins [[lein-monolith "0.3.2"]]}}
+  :plugins [[lein-monolith "0.3.2"]
+            [lein-cprint "1.2.0"]]
   :monolith {:project-dirs ["libs/*"]})
