e888131f8d6c89555a6d35f35aa94e8861e26b98
Boaz Rosenan <brosenan@gmail.com>
2017-04-10 08:19:32 +0300

    Deploy to GitHub Pages: 440fa8be240fff080379d6f0f103a2842f3f2262

---
+++ README.md
@@ -0,0 +1,5 @@
+# Axiom
+![build status](https://travis-ci.org/brosenan/axiom.svg?branch=master)
+
+This is the implementation of the axiom application platform
+See [our documentation](https://brosenan.github.io/cloudlog.clj/core.html) for details.
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject axiom "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-monolith "0.3.2"]
+            [lein-cprint "1.2.0"]]
+  :aliases {"im" ["do" ["midje"] ["install"]]
+            "eim" ["monolith" "each" "im"]}
+  :monolith {:project-dirs ["libs/*"]
+             :inherit [:aliases]})
