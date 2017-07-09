4a72dbc78e51baac5bb6d619c59f91ae6db6bc51
Boaz Rosenan <brosenan@gmail.com>
2017-07-09 19:04:30 +0300

    added im command

--- libs/axiom-cljs/project.clj
+++ libs/axiom-cljs/project.clj
@@ -1,6 +1,7 @@
 (defproject axiom-cljs "MONOLITH-SNAPSHOT"
   :description "A client library for Axiom"
   :url "http://axiom-clj.org"
+  :aliases {"im" ["doo" "phantom" "test" "once"]}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
