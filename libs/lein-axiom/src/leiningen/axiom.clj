94ade86a565f5ce1066e0aa71314215bad90a282
Boaz Rosenan <brosenan@gmail.com>
2017-07-29 15:20:06 +0300

    lein axiom deps

--- libs/lein-axiom/src/leiningen/axiom.clj
+++ libs/lein-axiom/src/leiningen/axiom.clj
@@ -1,6 +1,9 @@
 (ns leiningen.axiom
   (:require [di.core :as di]
-            [axiom-clj.core :as axiom]))
+            [axiom-clj.core :as axiom]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [permacode.publish :as publish]))
 
 
 (defn deploy
@@ -14,7 +17,8 @@
     (di/do-with! $ [deploy-dir publish println]
                  (deploy-dir ver "." publish)
                  (println (str "Deployed a new version: " ver)))
-    (di/shutdown $)))
+    (di/shutdown $)
+    (println "Add &_ver=" ver " to your URL")))
 
 (defn run
   "Run an Axiom instance"
@@ -25,10 +29,55 @@
     (while true
       (Thread/sleep 100))))
 
+(defn all-source-files [project]
+  (for [dir (:source-paths project)
+        file (file-seq (io/file dir))
+        :when (str/ends-with? (str file) ".clj")]
+    file))
+
+(defn required-perms [file]
+  (let [[_ns _name & clauses] (publish/get-ns file)]
+    (for [[_require & deps] clauses
+          [dep & args] deps
+          :when (str/starts-with? (str dep) "perm.")]
+      (-> dep
+          str
+          (subs 5)))))
+
+(defn create-perm-file [hashcode [hash unhash] src-dir]
+  (let [permdir (io/file src-dir "perm")
+        content (unhash hashcode)
+        file (io/file permdir (str hashcode ".clj"))]
+    (cond (-> file .exists)
+          false
+          :else
+          (do
+            (println (str file))
+            (-> permdir .mkdirs)
+            (with-open [file (io/writer file)]
+              (doseq [expr content]
+                (.write file (pr-str expr))))
+            true))))
+
+(defn deps
+  "Recursively add permacode dependencies to this project"
+  [project & args]
+  (let [$ (axiom/injector (:axiom-config project))
+        src-dir (io/file (first (:source-paths project)))]
+    (di/startup $)
+    (di/do-with! $ [hasher]
+                 (while (some identity
+                              (for [src (all-source-files project)
+                                    dep (required-perms src)]
+                                (create-perm-file dep hasher src-dir))))))
+  nil)
+
 (defn axiom
   "Automating common Axiom tasks"
-  {:subtasks [#'deploy #'run]}
+  {:subtasks [#'deploy #'run #'deps]}
   [project subtask & args]
   (let [task ({"deploy" deploy
-               "run" run} subtask)]
+               "run" run
+               "deps" deps} subtask)]
     (apply task project args)))
+
--- libs/lein-axiom/test/leiningen/axiom_test.clj
+++ libs/lein-axiom/test/leiningen/axiom_test.clj
@@ -1,16 +1,19 @@
 (ns leiningen.axiom-test
   (:require [midje.sweet :refer :all]
             [leiningen.axiom :refer :all]
-            [org.httpkit.client :as http]))
+            [org.httpkit.client :as http]
+            [clojure.java.io :as io]
+            [permacode.publish :as publish]))
 
 [[:chapter {:title "Introduction"}]]
 "`lein-axiom` is a [leiningen](https://leiningen.org) plugin for automating Axiom-related tasks.
 It provides the sub-tasks [deploy](#deploy) and [run](#run)."
 (fact
  (-> #'axiom meta :doc) => "Automating common Axiom tasks"
- (-> #'axiom meta :subtasks) => [#'deploy #'run]
+ (-> #'axiom meta :subtasks) => [#'deploy #'run #'deps]
  (-> #'deploy meta :doc) => "Deploy the contents of the project to the configured Axiom instance"
- (-> #'run meta :doc) => "Run an Axiom instance")
+ (-> #'run meta :doc) => "Run an Axiom instance"
+ (-> #'deps meta :doc) => "Recursively add permacode dependencies to this project")
 
 "The `lein-axiom` plugin requires an `:axiom-config` key to be present in the project.
 This configuration is used to initialize a [DI](di.html) [injector](di.html#injector) so that Axiom's components are available to the plugin."
@@ -48,3 +51,89 @@ It will remain running until interrupted (Ctrl+C) by the user."
    (let [res @(http/get "http://localhost:33333")]
      (:status res) => 200
      (-> res :body slurp) => "Hello")))
+
+[[:chapter {:title "deps"}]]
+"`.clj` files in Axiom apps are requied to be [valid permacode source files](permacode.validate.html#introduction).
+This means, among other things, that they [cannot `:require` any namespaces](permacode.validate.html#validate-ns), except for a small white-list of pure ones.
+The way to use external libraries (such as [cloudlog.core](cloudlog.html), which defines the `defrule` and `defclause` macros),
+one needs to `:require` them in the form `perm.*`, where `*` is replaced with a hash-code representing the dependency's content.
+Once deployed on Axiom, Axiom will look up these dependencies by their hash-code.
+However, for the purpose of running this code in the development environment we need a way to also add the dependencies to the project."
+
+"Running `lein axiom deps` will [look-up all `.clj` files in the project's `:source-paths`](#all-source-files),
+and for each such file will [extract the dependent `perm.*` namespaces](#required-perms).
+Then, it will use the hasher configured in `:axiom-config` to extract these dependencies and [write them to `.clj` files](#create-perm-file)
+in the project's *first source-path*, if they don't already exist."
+(fact
+ (let [project {:axiom-config {:hasher [..hash.. ..unhash..]}
+                :source-paths ["/path/to/proj/src1" "/path/to/proj/src2"]}]
+   (axiom project "deps") => nil
+   (provided
+    (all-source-files project) => [..src1.. ..src2..]
+    (required-perms ..src1..) => ["FOO" "BAR"]
+    (required-perms ..src2..) => ["BAZ"]
+    (io/file "/path/to/proj/src1") => ..src-dir..
+    (create-perm-file "FOO" [..hash.. ..unhash..] ..src-dir..) => false
+    (create-perm-file "BAR" [..hash.. ..unhash..] ..src-dir..) => false
+    (create-perm-file "BAZ" [..hash.. ..unhash..] ..src-dir..) => false)))
+
+"If new `perm.*` namespaces are introduced, `lein axiom deps` works iteratively to fetch their dependencies."
+(fact
+ (let [project {:axiom-config {:hasher [..hash.. ..unhash..]}
+                :source-paths ["/path/to/proj/src1"]}]
+   (axiom project "deps") => nil
+   (provided
+    (all-source-files project) => [..src1.. ..src2..]
+    (required-perms ..src1..) =streams=> [["FOO" "BAR"]
+                                          ["FOO" "BAR" "QUUX"]]
+    (required-perms ..src2..) => ["BAZ"]
+    (io/file "/path/to/proj/src1") => ..src-dir..
+    (create-perm-file "FOO" [..hash.. ..unhash..] ..src-dir..) => false
+    (create-perm-file "BAR" [..hash.. ..unhash..] ..src-dir..) =streams=> [true false]
+    (create-perm-file "BAZ" [..hash.. ..unhash..] ..src-dir..) => false
+    (create-perm-file "QUUX" [..hash.. ..unhash..] ..src-dir..) => false)))
+
+[[:chapter {:title "Under the Hood"}]]
+[[:section {:title "all-source-files"}]]
+"Given a `project`, `all-source-files` returns all the `*.clj` files located under all `:source-paths` in the `project`."
+(fact
+ (all-source-files {:source-paths ["/path/to/proj/src1"
+                                   "/path/to/proj/src2"]}) => [(io/file "/path/to/proj/src1/foo.clj")
+                                                               (io/file "/path/to/proj/src2/bar.clj")
+                                                               (io/file "/path/to/proj/src2/baz.clj")]
+ (provided
+  (file-seq (io/file "/path/to/proj/src1")) => [(io/file "/path/to/proj/src1/foo.clj")
+                                                (io/file "/path/to/proj/src1/x.cljs")
+                                                (io/file "/path/to/proj/src1/index.html")]
+  (file-seq (io/file "/path/to/proj/src2")) => [(io/file "/path/to/proj/src2/bar.clj")
+                                                (io/file "/path/to/proj/src2/baz.clj")]))
+
+[[:section {:title "required-perms"}]]
+"Given a file, `required-perms` returns all the permacode hash-codes required by that source file."
+(fact
+ (required-perms ..file..) => ["FOO" "BAR" "BAZ"]
+ (provided
+  (publish/get-ns ..file..) => '(ns some.ns
+                                  (:require [clojure.string :as str]
+                                            [perm.FOO :as foo])
+                                  (:require [perm.BAR]
+                                            [perm.BAZ :as baz]))))
+
+[[:section {:title "create-perm-file"}]]
+"Given a hash-code, a [hasher](permacode.hasher.html#introduction) and a source path,
+`create-perm-file` creates a `.clj` source file with the underlying content.
+The file will be placed under the `perm` directory because the namespace is named `perm.*` (where `*` is the given hash-code).
+It returns whether a new file needed to be created."
+(fact
+ (let [hasher [(fn [code content]
+                 (throw (Exception. "This should not be called")))
+               (fn [hashcode]
+                 '[(ns perm.FOO)
+                   (foo 123)
+                   (bar 234)])]
+       source-dir (io/file "/tmp" (str "testsrc-" (rand-int 100000)))]
+   (create-perm-file "FOO" hasher source-dir) => true
+   (-> (io/file source-dir "perm") .exists) => true
+   (-> (io/file source-dir "perm" "FOO.clj") slurp) => "(ns perm.FOO)(foo 123)(bar 234)"
+   (create-perm-file "FOO" hasher source-dir) => false ;; This file already exists
+   ))
